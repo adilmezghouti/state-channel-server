905aece45bc637b3bdc60b6c6250fe2d3632d61f
Adil Mezghouti <adilmezghouti@gmail.com>
2022-07-14 12:50:41 +0300

    Added support for cors and selective broadcasting

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+var cors = require('cors')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  socket.on('pay', (msg) => {
-    console.log('paying: ' + msg);
-    io.emit('paid', msg);
+  socket.on('transaction', (msg) => {
+    console.log('transaction: ' + msg);
+    // io.emit('transaction', msg);
+    socket.broadcast.emit('transaction', msg);
   });
 });
 server.listen(process.env.PORT, () => {
